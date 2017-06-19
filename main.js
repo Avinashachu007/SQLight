1c9d3e147287752299f26fe67320abd243b40297
Avinash <cuteavinash@ymail.com>
2017-06-19 16:52:01 +0530

    some changes to packages and node moduels

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ var app = electron.app;
 var browserWindow = electron.BrowserWindow;
 //mainWindow
 var mainWindow = null;
+//app ready function
+app.on('ready',function(){
+    mainWindow = new BrowserWindow({
+        height:700,
+        width:368
+    });
+    mainWindow.loadURL("file://"+__dirname+"/index.html");
+});
